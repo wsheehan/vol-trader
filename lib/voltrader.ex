4a545f65efa54428f3d9c5050ccd7e9e5fd1629e
Will Sheehan <willsheehan95@gmail.com>
2017-08-06 16:36:47 -0400

    Empty mix project

---
+++ lib/voltrader.ex
@@ -0,0 +1,18 @@
+defmodule Voltrader do
+  @moduledoc """
+  Documentation for Voltrader.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Voltrader.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/voltrader_test.exs
@@ -0,0 +1,8 @@
+defmodule VoltraderTest do
+  use ExUnit.Case
+  doctest Voltrader
+
+  test "greets the world" do
+    assert Voltrader.hello() == :world
+  end
+end
