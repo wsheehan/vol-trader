c381ce2f1bbd846bbf3b69d6ad4f81b174826bca
Will Sheehan <willsheehan95@gmail.com>
2017-08-07 11:57:01 -0400

    Bare trader agent

---
+++ lib/voltrader/trader.ex
@@ -0,0 +1,18 @@
+defmodule Voltrader.Trader do
+  @moduledoc """
+  The actual trader, isolated
+  instances that focus on the buying
+  and selling of one security flagged
+  by research
+  """
+
+  use Agent
+
+  @doc """
+  Starts a trading process
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> {} end)
+  end
+
+end
