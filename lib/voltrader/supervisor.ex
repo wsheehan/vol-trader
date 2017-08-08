bef82cba6b16a7af717e6d7d5c1bef4998a7ec1c
Will Sheehan <willsheehan95@gmail.com>
2017-08-08 12:02:48 -0400

    Test Quantum job :heavy_check_mark:

--- lib/voltrader/supervisor.ex
+++ lib/voltrader/supervisor.ex
@@ -1,7 +1,12 @@
 defmodule Voltrader.Supervisor do
+  @moduledoc """
+  Master Supervisor started on Voltrader.start()
+  """
+
   use Supervisor
 
   alias Voltrader.Registry
+  alias Voltrader.Scheduler
 
   def start_link(opts) do
     Supervisor.start_link(__MODULE__, :ok, opts)
@@ -10,7 +15,11 @@ defmodule Voltrader.Supervisor do
   def init(:ok) do
     children = [
       Voltrader.TraderSupervisor,
-      {Registry, name: Registry}
+      {Registry, name: Registry},
+      %{
+        id: Scheduler,
+        start: {Scheduler, :start_link, []}
+      }
     ]
 
     Supervisor.init(children, strategy: :one_for_all)
