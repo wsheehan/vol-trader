78d75a7b12610941e1a67f7dd41f73533e63a848
Will Sheehan <willsheehan95@gmail.com>
2017-08-10 13:07:40 -0400

    Refactor PriceSocket

--- lib/voltrader/data/price_socket.ex
+++ lib/voltrader/data/price_socket.ex
@@ -6,19 +6,13 @@ defmodule Voltrader.Data.PriceSocket do
 
   use Agent
 
+  @slug %{url: "api.bitfinex.com", path: "/ws/2"}
+
   def start_link(_opts) do
-    Agent.start_link(fn -> connect() end, name: :master_socket)
+    Agent.start_link(fn -> connect() end, name: __MODULE__)
   end
 
   defp connect do
-    socket = Socket.Web.connect! slug().url, path: slug().path, secure: true
-    # case socket |> Socket.Web.recv! do
-    #   {:text, _} ->
-    #     IO.puts "RESPONSE"
-    # end
-  end
-
-  defp slug do
-    %{url: "api.bitfinex.com", path: "/ws/2"}
+    Socket.Web.connect! @slug.url, path: @slug.path, secure: true
   end
 end
