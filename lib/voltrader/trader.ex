4a70d54837829f95b5fc2d5500b86d7990eb94d3
Will Sheehan <willsheehan95@gmail.com>
2017-08-11 09:53:32 -0400

    Remove uneccessary trader functions

--- lib/voltrader/trader.ex
+++ lib/voltrader/trader.ex
@@ -14,36 +14,4 @@ defmodule Voltrader.Trader do
   def start_link(_opts) do
     Agent.start_link(fn -> open_channel() end)
   end
-
-  @doc """
-  Opens trader specific channel
-  """
-  def open_channel do
-    {:ok, msg} = Poison.encode(%{event: "subscribe", channel: "ticker", symbol: "tBTCUSD"})
-    Agent.get(Voltrader.Data.PriceSocket, fn socket ->
-      case socket |> Socket.Web.send!({:text, msg}) do
-        :ok ->
-          IO.puts "CHANNEL CONNECTION"
-          listen(socket)
-      end
-    end)
-    %{ticker: "tBTCUSD"}
-  end
-
-  defp listen(socket) do
-    case socket |> Socket.Web.recv! do
-      {:text, text} ->
-        resp = Poison.decode!(text)
-        IO.puts "RESPONSE"
-      # _ ->
-      #   IO.puts "UNMATCHED"
-    end
-    listen(socket)
-  end
-
-  # case socket |> Socket.Web.recv! do
-  #   {:text, _} ->
-  #     IO.puts "RESPONSE"
-  # end
-
 end
