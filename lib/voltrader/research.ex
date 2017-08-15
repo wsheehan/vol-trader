66e13de9df4064c419e710498e5128ddff747c11
Will Sheehan <willsheehan95@gmail.com>
2017-08-15 16:26:24 -0400

    Use bfinex candles for research

--- lib/voltrader/research.ex
+++ lib/voltrader/research.ex
@@ -6,32 +6,36 @@ defmodule Voltrader.Research do
 
   alias Voltrader.Socket.BitfinexClient
   alias Voltrader.Trader.Registry
+  alias Voltrader.Utilities
 
+  @candle_labels ~w(MTS OPEN CLOSE HIGH LOW VOLUME)
   @bitfinex_tickers %{"BTC" => "tBTCUSD", "ETH" => "tETHUSD", "OMG" => "tOMGUSD", 
-  "LTC" => "tLTCUSD", "EOS" => "tEOSUSD", "DASH" => "tDASHUSD", "XMR" => "tXMRUSD",
-  "BCH" => "tBCUUSD", "ZEC" => "tZECUSD", "XRP" => "tXRPUSD", "SAN" => "tSANUSD"}
+  "LTC" => "tLTCUSD", "EOS" => "tEOSUSD", "XMR" => "tXMRUSD", "BCH" => "tBCUUSD", 
+  "ZEC" => "tZECUSD", "XRP" => "tXRPUSD", "SAN" => "tSANUSD"}
 
   def call do
-    HTTPoison.get!("https://api.coinmarketcap.com/v1/ticker/").body
-    |> Poison.decode!
-    |> Enum.filter(fn(map) -> Map.has_key?(@bitfinex_tickers, map["symbol"]) end)
-    |> Enum.each(fn(x) -> set_targets(x) end)
+    Enum.each(@bitfinex_tickers, fn({_, ticker}) ->
+      HTTPoison.get!("https://api.bitfinex.com/v2/candles/trade:1h:#{ticker}/last").body
+      |> Poison.decode!
+      |> Utilities.lists_to_map(@candle_labels)
+      |> set_targets(ticker)
+    end)
   end
 
-  defp set_targets(data) do
-    case String.to_float(data["percent_change_1h"]) do
-      del when del > 0 ->
-        price = String.to_float(data["price_usd"])
-        targets = %{sell_stop: price * 0.95, buy_stop: price, target_sell: price * 1.02, volume: 10}
-        start_trader(targets, @bitfinex_tickers[data["symbol"]])
-      del when del >= 0 ->
-        IO.puts("No bite")
+  defp set_targets(data, ticker) do
+    percent_change = (data["CLOSE"] - data["OPEN"]) / data["OPEN"]
+    case percent_change do
+      x when x > 0.01 -> # Incredibly naive at this point
+        targets = %{sell_stop: data["CLOSE"] * 0.9, target_sell: data["CLOSE"] * 1.05, volume: 10}
+        start_trader(targets, ticker)
+      _ ->
+        IO.puts("Not buying #{ticker}")
     end
   end
 
   defp start_trader(targets, ticker) do
     BitfinexClient.join_channel(BitfinexClient, ticker)
     {:ok, trader} = Registry.lookup(Registry, ticker, BitfinexClient)
-    Process.send(trader, {:targets, targets}, [])
+    Process.send(trader, {:targets, targets, ticker}, [])
   end
 end
