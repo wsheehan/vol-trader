ddfd566a1749670a9134acc346f794be6810d686
Will Sheehan <willsheehan95@gmail.com>
2017-08-15 11:34:27 -0400

    Trader handle incoming quotes

--- lib/voltrader/trader.ex
+++ lib/voltrader/trader.ex
@@ -24,4 +24,26 @@ defmodule Voltrader.Trader do
     {:ok, state}
   end
 
+  @doc """
+  Handles quotes in list form
+  """
+  def handle_info({:quote, data}, state) when is_list(data) do
+    IO.inspect(data)
+    {:noreply, state}
+  end
+
+  @doc """
+  Handles price target change
+  """
+  def handle_info({:targets, data}, state) do
+    {:noreply, state}
+  end
+
+  @doc """
+  Handles responses we dont care about
+  e.g. BitFinex sends "hb" sometimes
+  """
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
 end
