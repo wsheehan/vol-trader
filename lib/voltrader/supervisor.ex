3270920a35118c60b60bdfad22645215e17f9d21
Will Sheehan <willsheehan95@gmail.com>
2017-08-16 14:40:47 -0400

    Start Ecto.Repo

--- lib/voltrader/supervisor.ex
+++ lib/voltrader/supervisor.ex
@@ -13,7 +13,8 @@ defmodule Voltrader.Supervisor do
       {Voltrader.Trader.Supervisor, name: Voltrader.Trader.Supervisor},
       {Voltrader.Trader.Registry, name: Voltrader.Trader.Registry},
       {Voltrader.Socket.BitfinexClient, name: Voltrader.Socket.BitfinexClient},
-      {Voltrader.Socket.IEXClient, name: Voltrader.Socket.IEXClient}
+      {Voltrader.DB.Repo, {Voltrader.DB.Repo, :start_link, []}, :permanent, :infinity,
+ :supervisor, [Voltrader.DB.Repo]}
     ]
 
     Supervisor.init(children, strategy: :one_for_all)
