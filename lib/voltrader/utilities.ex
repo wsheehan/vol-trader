3acf68a41a344ba8c62d529bce22ee0e70acaa14
Will Sheehan <willsheehan95@gmail.com>
2017-08-17 13:04:11 -0400

    Current time utility fn

--- lib/voltrader/utilities.ex
+++ lib/voltrader/utilities.ex
@@ -1,9 +1,15 @@
 defmodule Voltrader.Utilities do
+  use Timex
+
   @doc """
   Joins two lists into key value map
   second value is `label` list
   """
   def lists_to_map(l1, l2) do
     Enum.zip(l2, l1) |> Enum.into(%{})
+  end
+
+  def current_time do
+    Timex.now |> Timex.format!("{ISO:Extended}")
   end
 end
